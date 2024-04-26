e59db0572a81d0ed7ea031fe4aca373473db0523
Rashid Holder <rashid.m.holder@gmail.com>
2024-04-26 08:19:51 -0400

    Initial commit from tutorial

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
-  ...styleguide,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+	...styleguide,
+	tabWidth: 2,
+	tabSize: 2,
+	useTabs: true,
+	printWidth: 120,
+	plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
